93eb8191d2028a7be952da9a10b3c6aa95a3edd1
Anythingsoup01 <williamenglish1220@gmail.com>
2025-06-30 09:28:20 -0400

    Got Initial WM Working

---
+++ BearWM/src/BearWM/Core/BearWM.cpp
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <X11/Xlib.h>
+#include <X11/cursorfont.h>
+
+Display* display;
+Window window;
+
+void grabKey(char* key, unsigned int mod)
+{
+    KeySym sym = XStringToKeysym(key);
+    KeyCode code = XKeysymToKeycode(display, sym);
+    XGrabKey(display, code, mod, window, False, GrabModeAsync, GrabModeAsync);
+    XSync(display, False);
+}
+
+bool GetKeyCode(XEvent* e, char* key, unsigned int mod)
+{
+    KeySym sym = XStringToKeysym(key);
+    return e->xkey.keycode == XKeysymToKeycode(display, sym);
+}
+
+int main(int argc, char* argv[])
+{
+    display = XOpenDisplay(NULL);
+    if (display == NULL)
+    {
+        printf("Failed to open X Display");
+        return EXIT_FAILURE;
+    }
+
+    window = DefaultRootWindow(display);
+
+    XSelectInput(display, window, SubstructureRedirectMask | SubstructureNotifyMask);
+
+    Cursor cursor = XCreateFontCursor(display, XC_left_ptr);
+
+    XDefineCursor(display, window, cursor);
+
+    XGrabButton(display, Button1, 0, window, 0, ButtonPressMask, GrabModeSync, GrabModeAsync, NULL, NULL);
+
+    grabKey("a", ShiftMask | ControlMask);
+    grabKey("s", ShiftMask | ControlMask);
+
+    XSync(display, False);
+
+    XEvent e;
+    for(;;)
+    {
+        XNextEvent(display, &e);
+        switch(e.type)
+        {
+            case ButtonPress:
+            {
+                XAllowEvents(display, ReplayPointer, CurrentTime);
+                XSync(display, 0);
+                break;
+            }
+            case KeyPress:
+            {
+                if (GetKeyCode(&e, "a", ShiftMask | ControlMask))
+                {
+                    return EXIT_SUCCESS;
+                }
+                break;
+            }
+            default:
+            {
+                break;
+            }
+        }
+
+        XSync(display, 0);
+    }
+
+
+    XCloseDisplay(display);
+
+    return EXIT_SUCCESS;
+}
